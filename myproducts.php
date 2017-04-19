9d621d7fa2b709d6f95c7e249d11a2c4588b7c35
Abdelrahman Mohamed <MrAbdelrahman10@gmail.com>
2017-04-19 14:44:03 +0300

    Create main plugin's file and add file description

---
+++ myproducts.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+    Plugin Name: My Products
+    Plugin URI: http://www.mrabdelrahman10.com
+    Description: Plugin for controlling products from an OSCommerce shopping cart
+    Author: Abdelrahman Mohamed
+    Version: 1.0
+    Author URI: http://www.mrabdelrahman10.com
+*/
+
