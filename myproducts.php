485789b2d1a861bc58e0ca84f0943fe04e81ff17
Abdelrahman Mohamed <MrAbdelrahman10@gmail.com>
2017-04-21 19:08:29 +0300

    * Delete install and activate functions * Delete inc.php, main.php, myproducts_add.php and style.css * Update function render_myproduct_shortcode to get product as custom post type * Added Function "product_register" to define product as Custom Post Type * Added Function "admin_init" to add meta box * Added Function "product_meta" to Render elements for Product * Added Function "save_product" to Save product meta data in database

--- myproducts.php
+++ myproducts.php
@@ -4,64 +4,9 @@
   Plugin URI: http://www.mrabdelrahman10.com
   Description: Plugin for controlling products from an OSCommerce shopping cart
   Author: Abdelrahman Mohamed
-  Version: 1.0
+  Version: 2.0
   Author URI: http://www.mrabdelrahman10.com
  */
-
-/**
- * Including main plugin's file
- */
-function my_products_admin() {
-    include('inc.php');
-    include('main.php');
-}
-
-/**
- * Including form plugin's file to add and edit
- */
-function myproducts_add() {
-    include('inc.php');
-    include('myproducts_add.php');
-}
-
-/**
- * Adding "My Products" to menu
- */
-function my_products_admin_actions() {
-    add_menu_page("My Products Display", "My Products", 1, "my_products", "my_products_admin");
-
-    //this is a submenu
-    add_submenu_page('my_products', 'Add New Product', 'Add New', 'manage_options', 'myproducts_add', 'myproducts_add');
-}
-
-add_action('admin_menu', 'my_products_admin_actions');
-
-/**
- * Creating necessary tables
- * @global string $table_prefix
- * @global string $wpdb
- */
-function create_plugin_database_table() {
-    global $table_prefix, $wpdb;
-
-    $tblname = 'my_product';
-    $wp_track_table = $table_prefix . "$tblname";
-
-    #Check to see if the table exists already, if not, then create it
-    if ($wpdb->get_var("show tables like '$wp_track_table'") != $wp_track_table) {
-        $sql = "CREATE TABLE `{$wp_track_table}` (
-                `id` INT NOT NULL AUTO_INCREMENT,
-                `title` VARCHAR(100) NOT NULL,
-                `description` TEXT NOT NULL,
-                `sku` VARCHAR(100) NOT NULL, PRIMARY KEY (`id`)
-                ) COLLATE='utf8_general_ci' ENGINE=InnoDB;";
-        require_once(ABSPATH . '/wp-admin/upgrade-functions.php');
-        dbDelta($sql);
-        add_option('myproducts_version', '1.0');
-    }
-}
-
-register_activation_hook(__FILE__, 'create_plugin_database_table');
 
 /**
  * Uninstalling tables of plugin
@@ -71,9 +16,9 @@ register_activation_hook(__FILE__, 'create_plugin_database_table');
 function uninstall_plugin_database_table() {
     global $table_prefix, $wpdb;
 
-    $tblname = 'my_product';
+    $tblname = 'postmeta';
     $wp_track_table = $table_prefix . "$tblname ";
-    $sql = "DROP TABLE IF EXISTS `{$wp_track_table}`";
+    $sql = "Delete From `{$wp_track_table}` Where IN ('my_description', 'my_sku')";
     require_once(ABSPATH . '/wp-admin/upgrade-functions.php');
     dbDelta($sql);
     delete_option('myproducts_version');
@@ -87,32 +32,138 @@ register_uninstall_hook(__FILE__, 'uninstall_plugin_database_table');
  * @global string $wpdb
  */
 function render_myproduct_shortcode() {
-    global $table_prefix, $wpdb;
-    $sql = "SELECT * FROM {$table_prefix}my_product Order By id DESC";
-    $rows = $wpdb->get_results($sql) or die(mysql_error());
+    global $wpdb;
+    $sql = "
+            SELECT DISTINCT wposts.*, wpostmeta.meta_value AS my_sku
+        FROM $wpdb->posts wposts
+	INNER JOIN $wpdb->postmeta wpostmeta ON wposts.ID = wpostmeta.post_id
+        WHERE wpostmeta.meta_key = 'my_sku'
+        ";
+    $rows = $wpdb->get_results($sql);
+    if($rows){
     ?>
     <table>
         <thead>
-        <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Sku</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach ($rows as $row) {
-        ?>
             <tr>
-                <td><?php echo $row->id; ?></td>
-                <td><?php echo $row->title; ?></td>
-                <td><?php echo $row->sku; ?></td>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Sku</th>
             </tr>
-        <?php
-    }
-    ?>
-    </tbody>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($rows as $row) {
+                ?>
+                <tr>
+                    <td><?php echo $row->ID; ?></td>
+                    <td><a href="<?php echo get_permalink($row->ID); ?>"><?php echo $row->post_title; ?></a></td>
+                    <td><?php echo $row->my_sku; ?></td>
+                </tr>
+                <?php
+            }
+            ?>
+        </tbody>
     </table>
     <?php
+} else {
+    echo 'No Products';
 }
+}
+/**
+ * Call it to active Shortcode
+ */
 add_shortcode('myproduct', 'render_myproduct_shortcode');
+
+
+/**
+ * Function to define product as Custom Post Type
+ */
+function product_register() {
+
+    $labels = array(
+        'name' => _x('My Products', 'Post type name'),
+        'singular_name' => _x('Product Item', 'Post type singular name'),
+        'add_new' => _x('Add New', 'Product item'),
+        'add_new_item' => __('Add New Product Item'),
+        'edit_item' => __('Edit Product Item'),
+        'new_item' => __('New Product Item'),
+        'view_item' => __('View Product Item'),
+        'search_items' => __('Search Product'),
+        'not_found' => __('Nothing found'),
+        'not_found_in_trash' => __('Nothing found in Trash'),
+        'parent_item_colon' => ''
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'menu_position' => null,
+        'supports' => array('title', 'editor', 'thumbnail')
+    );
+
+    register_post_type('product', $args);
+}
+/**
+ * Register Product plugin as Custom Post Type
+ */
+add_action('init', 'product_register');
+
+
+/**
+ * Function to define product meta
+ */
+function admin_init() {
+    add_meta_box("product_meta", "Product Details", "product_meta", "product", "normal", "low");
+}
+/**
+ * Add it to admin
+ */
+add_action("admin_init", "admin_init");
+
+/**
+ * Render elements for Product
+ */
+function product_meta() {
+    global $post;
+    $custom = get_post_custom($post->ID);
+    $sku = $custom["my_sku"][0];
+    $description = $custom["my_description"][0];
+    ?>
+    <style>
+        #product-form input, #product-form textarea{
+            width: 100%;
+        }
+    </style>
+    <div id="product-form">
+        <p>
+            <label>Sku :</label><br />
+            <input type="text" name="sku" value="<?php echo $sku; ?>" />
+        </p>
+        <p>
+            <label>Description :</label><br />
+            <textarea cols="50" rows="5" name="description"><?php echo $description; ?></textarea>
+        </p>
+    </div>
+    <?php
+}
+
+/**
+ * Save product meta data
+ */
+function save_product() {
+    global $post;
+
+    update_post_meta($post->ID, "my_description", $_POST["description"]);
+    update_post_meta($post->ID, "my_sku", $_POST["sku"]);
+}
+
+/**
+ * Call it to save product meta data
+ */
+add_action('save_post', 'save_product');
